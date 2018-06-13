48a9867a6269035e535e558ecc0194946aa4cb79
liberty2016 <409947972@qq.com>
2018-06-13 08:22:39 +0800

    feat(comment): 添加平衡二叉树的相关注释

--- src/BalancedBinarySearchTreeDemo.java
+++ src/BalancedBinarySearchTreeDemo.java
@@ -56,13 +56,13 @@ public class BalancedBinarySearchTreeDemo {
         TreeNode l,lr;
         l=tree.lNode;
         switch (l.bf){
-            case LH:
+            case LH:/*新节点插入在tree的左孩子的左子树上，要作单右旋操作**/
                 tree.bf=l.bf=EH;
                 tree=R_Rotate(tree);
                 break;
-            case RH:
+            case RH:/*新节点插入在tree的左孩子的右子树上，要作双旋操作*/
                 lr=l.rNode;
-                switch (lr.bf){
+                switch (lr.bf){/*根据tree的左孩子的右子树的平衡因子修改tree以及其左孩子的平衡因子*/
                     case LH:
                         tree.bf=RH;
                         l.bf=EH;
@@ -92,13 +92,13 @@ public class BalancedBinarySearchTreeDemo {
         TreeNode r,rl;
         r=tree.rNode;
         switch (r.bf){
-            case RH:
+            case RH:/*新节点插入在tree的右孩子的右子树上，要作单左旋操作**/
                 tree.bf=r.bf=EH;
                 tree=L_Rotate(tree);
                 break;
-            case LH:
+            case LH:/*新节点插入在tree的右孩子的左子树上，要作双旋操作*/
                 rl=r.lNode;
-                switch (rl.bf){
+                switch (rl.bf){/*根据tree的右孩子的左子树的平衡因子修改tree以及其右孩子的平衡因子*/
                     case LH:
                         tree.bf=EH;
                         r.bf=RH;
@@ -146,15 +146,15 @@ public class BalancedBinarySearchTreeDemo {
                 tree.lNode=n;
                 if (taller.taller){//已插入到左子树中并且左子树“长高”
                     switch (tree.bf){
-                        case LH:
+                        case LH:/*原本tree的左子树比右子树高，现左子树更高，需要作左平衡处理*/
                             tree=LeftBalance(tree);
                             taller.taller=false;
                             break;
-                        case EH:
+                        case EH:/*原本左右子树等高，现因左子树增高而树增高*/
                             tree.bf=LH;
                             taller.taller=true;
                             break;
-                        case RH:
+                        case RH:/*原本右子树比左子树高，现左右字数等高*/
                             tree.bf=EH;
                             taller.taller=false;
                             break;
@@ -166,17 +166,17 @@ public class BalancedBinarySearchTreeDemo {
                     return null;
                 }
                 tree.rNode=n;
-                if (taller.taller){
+                if (taller.taller){//已插入到右子树中并且右子树“长高”
                     switch (tree.bf){
-                        case LH:
+                        case LH:/*原本右子树比左子树高，现左右字数等高*/
                             tree.bf=EH;
                             taller.taller=false;
                             break;
-                        case EH:
+                        case EH:/*原本左右子树等高，现右子树增高而树增高*/
                             tree.bf=RH;
                             taller.taller=true;
                             break;
-                        case RH:
+                        case RH:/*原本tree的右子树比左子树高，现右子树更高，需要作右平衡处理*/
                             tree=RightBalance(tree);
                             taller.taller=false;
                             break;
