363764afb7dff497095214f0e6275ac77b775d8d
Liberty <409947972@qq.com>
2019-01-01 18:51:12 +0800

    LruBasedLinkedList

---
+++ src/linearTable/linkedList/LRUBasedLinkedList.java
@@ -0,0 +1,21 @@
+package linearTable.linkedList;
+
+/**
+ * Created by Administrator on 2019/1/1.
+ */
+public class LRUBasedLinkedList<T> {
+
+    private TreeNode first;
+    private TreeNode last;
+
+    class TreeNode {
+        T data;
+        TreeNode next;
+    }
+
+
+
+    public LRUBasedLinkedList(int capacity){
+
+    }
+}
